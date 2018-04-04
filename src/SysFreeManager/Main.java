e59ea4192789fd7d2ffb6c54a8b6c1c8bfaa97be
ItsCosmas <gikunjucosmas@gmail.com>
2018-04-04 22:10:39 +0300

    Removed Windows Decorators from login Added some icons Added the Add Media scene Removed the Mark Featured media scene Grouped files into folders Added database connection with MYSQL DB

---
+++ src/SysFreeManager/Main.java
@@ -0,0 +1,27 @@
+package SysFreeManager;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+
+        Parent root = FXMLLoader.load(getClass().getResource("UserInterface/Login.fxml"));
+        stage.setTitle("Free Management System");
+        stage.setScene(new Scene(root));
+        stage.setResizable(false);
+        //stage.initStyle(StageStyle.UNDECORATED);
+        stage.show();
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}

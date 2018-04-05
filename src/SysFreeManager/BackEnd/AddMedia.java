2d20fa44d0a06b0f608f98cf60c62ea9d794a702
ItsCosmas <gikunjucosmas@gmail.com>
2018-04-05 23:07:04 +0300

    Handling different Scene Switches

--- src/SysFreeManager/BackEnd/AddMedia.java
+++ src/SysFreeManager/BackEnd/AddMedia.java
@@ -1,4 +1,65 @@
 package SysFreeManager.BackEnd;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class AddMedia {
+
+    @FXML
+
+    private void goToUserManagement (ActionEvent event) throws Exception{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/SysFreeManager/UserInterface/UserManagement.fxml"));
+        Parent home = loader.load();
+        Scene home_scene = new Scene(home);
+        Stage home_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        home_stage.setScene(home_scene);
+        home_stage.show();
+    }
+
+
+    @FXML
+
+    private void goToAddUser(ActionEvent event) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/SysFreeManager/UserInterface/AddUser.fxml"));
+        Parent home = loader.load();
+        Scene home_scene = new Scene(home);
+        Stage home_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        home_stage.setScene(home_scene);
+        home_stage.show();
+
+    }
+
+    @FXML
+
+    private void goToDashboard(ActionEvent event) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/SysFreeManager/UserInterface/Dashboard.fxml"));
+        Parent home = loader.load();
+        Scene home_scene = new Scene(home);
+        Stage home_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        home_stage.setScene(home_scene);
+        home_stage.show();
+
+    }
+
+    @FXML
+
+    private void goToHome(ActionEvent event) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/SysFreeManager/UserInterface/Home.fxml"));
+        Parent home = loader.load();
+        Scene home_scene = new Scene(home);
+        Stage home_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        home_stage.setScene(home_scene);
+        home_stage.show();
+
+    }
+
 }
